671e3c8d385edc71a7f599fa55590637d2641b95
Sanzhai01 <santsi02@mail.ru>
2025-06-13 11:45:02 +0900

    создает файл index.php

--- index.php
+++
@@ -1,8 +0,0 @@
-<?php
-$conn = @pg_connect("host=postgres_db dbname=mydb user=user password=pass"); // @ подавляет предупреждения
-if ($conn) {
-    echo "Connected to PostgreSQL successfully!";
-} else {
-    echo "Connection failed: " . pg_last_error();
-}
-?>
